ebc436a5a97f0fea959b778ecf77e261f92df8ca
arita37 <noelkev0@gmail.com>
2020-05-09 16:11:20 +0000

    ml_store

--- log_testall/log_testall_2020-05-09-16-11_c650f5b1ef2efd9067a12b489479a213849a404f.py
+++ log_testall/log_testall_2020-05-09-16-11_c650f5b1ef2efd9067a12b489479a213849a404f.py
@@ -63,3 +63,109 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 0cc058c] ml_store
+ 1 file changed, 65 insertions(+)
+ create mode 100644 log_testall/log_testall_2020-05-09-16-11_c650f5b1ef2efd9067a12b489479a213849a404f.py
+To github.com:arita37/mlmodels_store.git
+   f14646f..0cc058c  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tf//1_lstm.py 
+WARNING:tensorflow:From /opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+start
+
+  #### Loading params   ############################################## 
+
+  ############# Data, Params preparation   ################# 
+
+  {'learning_rate': 0.001, 'num_layers': 1, 'size': 6, 'size_layer': 128, 'timestep': 4, 'epoch': 2, 'output_size': 6} {'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv', 'data_type': 'pandas'} {} {'path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/', 'model_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/model'} 
+
+  #### Loading dataset   ############################################# 
+{'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv', 'data_type': 'pandas'}
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv
+         Date        Open        High  ...       Close   Adj Close   Volume
+0  2016-11-02  778.200012  781.650024  ...  768.700012  768.700012  1872400
+1  2016-11-03  767.250000  769.950012  ...  762.130005  762.130005  1943200
+2  2016-11-04  750.659973  770.359985  ...  762.020020  762.020020  2134800
+3  2016-11-07  774.500000  785.190002  ...  782.520020  782.520020  1585100
+4  2016-11-08  783.400024  795.632996  ...  790.510010  790.510010  1350800
+
+[5 rows x 7 columns]
+
+  #### Model init  ############################################# 
+
+  #### Model fit   ############################################# 
+{'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv', 'data_type': 'pandas'}
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv
+         Date        Open        High  ...       Close   Adj Close   Volume
+0  2016-11-02  778.200012  781.650024  ...  768.700012  768.700012  1872400
+1  2016-11-03  767.250000  769.950012  ...  762.130005  762.130005  1943200
+2  2016-11-04  750.659973  770.359985  ...  762.020020  762.020020  2134800
+3  2016-11-07  774.500000  785.190002  ...  782.520020  782.520020  1585100
+4  2016-11-08  783.400024  795.632996  ...  790.510010  790.510010  1350800
+
+[5 rows x 7 columns]
+          0         1         2         3         4         5
+0  0.706562  0.629914  0.682052  0.599302  0.599302  0.153665
+1  0.458824  0.320251  0.598101  0.478596  0.478596  0.174523
+2  0.083484  0.331101  0.437246  0.476576  0.476576  0.230969
+3  0.622851  0.723606  0.854891  0.853206  0.853206  0.069025
+4  0.824209  1.000000  1.000000  1.000000  1.000000  0.000000
+
+  #### Predict   ##################################################### 
+{'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv', 'data_type': 'pandas', 'train': 0}
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year.csv
+         Date        Open        High  ...       Close   Adj Close   Volume
+0  2016-11-02  778.200012  781.650024  ...  768.700012  768.700012  1872400
+1  2016-11-03  767.250000  769.950012  ...  762.130005  762.130005  1943200
+2  2016-11-04  750.659973  770.359985  ...  762.020020  762.020020  2134800
+3  2016-11-07  774.500000  785.190002  ...  782.520020  782.520020  1585100
+4  2016-11-08  783.400024  795.632996  ...  790.510010  790.510010  1350800
+
+[5 rows x 7 columns]
+          0         1         2         3         4         5
+0  0.706562  0.629914  0.682052  0.599302  0.599302  0.153665
+1  0.458824  0.320251  0.598101  0.478596  0.478596  0.174523
+2  0.083484  0.331101  0.437246  0.476576  0.476576  0.230969
+3  0.622851  0.723606  0.854891  0.853206  0.853206  0.069025
+4  0.824209  1.000000  1.000000  1.000000  1.000000  0.000000
+5  0.745928  0.883387  0.838176  0.904464  0.904464  0.370110
+6  1.000000  0.881878  0.467996  0.486496  0.486496  1.000000
+7  0.216516  0.077549  0.433808  0.329598  0.329598  0.318466
+8  0.195249  0.000000  0.000000  0.000000  0.000000  0.671960
+9  0.000000  0.173783  0.369041  0.411721  0.411721  0.304384
+
+  #### metrics   ##################################################### 
+{'loss': 0.42204469442367554, 'loss_history': []}
+
+  #### Plot   ######################################################## 
+
+  #### Save/Load   ################################################### 
+{'path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/', 'model_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/model'}
+{'path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/', 'model_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/ztest/model_tf/1_lstm/model'}
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+Already up to date.
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
