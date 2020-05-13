cc9c97de5e2b97a9645fa9361290941c504fc72a
arita37 <noelkev0@gmail.com>
2020-05-13 00:43:53 +0000

    ml_store

--- log_testall/log_testall_2020-05-13-00-15_6672e19fe4cfa7df885e45d91d645534b8989485.py
+++ log_testall/log_testall_2020-05-13-00-15_6672e19fe4cfa7df885e45d91d645534b8989485.py
@@ -10514,3 +10514,40 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 70c9376] ml_store
+ 1 file changed, 1122 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   c15dcfb..70c9376  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_classifier.py 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_classifier.py", line 522, in <module>
+    model_pars, data_pars, compute_pars, out_pars = get_params(param_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_classifier.py", line 418, in get_params
+    cf = json.load(open(data_path, mode='r'))
+FileNotFoundError: [Errno 2] No such file or directory: 'model_tch/transformer_classifier.json'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+Already up to date.
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
