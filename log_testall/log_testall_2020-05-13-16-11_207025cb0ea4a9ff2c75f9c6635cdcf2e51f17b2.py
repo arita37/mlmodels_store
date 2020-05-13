32569a311a8913e347f9f37da7bee7e434193c26
arita37 <noelkev0@gmail.com>
2020-05-13 17:01:34 +0000

    ml_store

--- log_testall/log_testall_2020-05-13-16-11_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+++ log_testall/log_testall_2020-05-13-16-11_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
@@ -11045,3 +11045,55 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master c3454b0] ml_store
+ 1 file changed, 35 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   9d9c895..c3454b0  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py 
+
+  #### Loading params   ############################################## 
+
+  #### Path params   ########################################## 
+
+  #### Loading dataset   ############################################# 
+
+  #### Model init, fit   ############################################# 
+Epoch:   0%|          | 0/1 [00:00<?, ?it/s]
+Iteration:   0%|          | 0/29440 [00:00<?, ?it/s][A
+Iteration:   0%|          | 1/29440 [00:14<122:18:49, 14.96s/it][A
+Iteration:   0%|          | 2/29440 [01:07<214:31:09, 26.23s/it][A
+Iteration:   0%|          | 3/29440 [02:28<348:45:42, 42.65s/it][AKilled
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+From github.com:arita37/mlmodels_store
+   c3454b0..08d6fae  master     -> origin/master
+Updating c3454b0..08d6fae
+Fast-forward
+ .../20200513/list_log_dataloader_20200513.md       |    2 +-
+ error_list/20200513/list_log_jupyter_20200513.md   | 2368 ++++++++++----------
+ error_list/20200513/list_log_test_cli_20200513.md  |  378 ++--
+ error_list/20200513/list_log_testall_20200513.md   |   30 +
+ 4 files changed, 1420 insertions(+), 1358 deletions(-)
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
