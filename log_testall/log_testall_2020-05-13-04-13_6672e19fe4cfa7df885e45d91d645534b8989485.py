d6f26b63858b06c7038d5fca81181a3bf4585d96
arita37 <noelkev0@gmail.com>
2020-05-13 04:30:16 +0000

    ml_store

--- log_testall/log_testall_2020-05-13-04-13_6672e19fe4cfa7df885e45d91d645534b8989485.py
+++ log_testall/log_testall_2020-05-13-04-13_6672e19fe4cfa7df885e45d91d645534b8989485.py
@@ -6155,3 +6155,47 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 9990dee] ml_store
+ 1 file changed, 48 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   3127bec..9990dee  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py 
+
+  #### Loading params   ############################################## 
+
+  #### Loading dataset   ############################################# 
+Using TensorFlow backend.
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py", line 348, in <module>
+    test(pars_choice="json", data_path=f"model_keras/namentity_crm_bilstm.json")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py", line 311, in test
+    Xtuple = get_dataset(data_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py", line 193, in get_dataset
+    raise Exception(f"Not support dataset yet")
+Exception: Not support dataset yet
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+Already up to date.
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
