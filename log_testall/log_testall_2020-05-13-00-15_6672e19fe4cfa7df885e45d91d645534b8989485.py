8aa6bee9712caff780116b68a6f5ecbf5111091d
arita37 <noelkev0@gmail.com>
2020-05-13 02:15:59 +0000

    ml_store

--- log_testall/log_testall_2020-05-13-00-15_6672e19fe4cfa7df885e45d91d645534b8989485.py
+++ log_testall/log_testall_2020-05-13-00-15_6672e19fe4cfa7df885e45d91d645534b8989485.py
@@ -10865,3 +10865,57 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 65684cb] ml_store
+ 1 file changed, 35 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   f695ad3..65684cb  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//pplm.py 
+ Generating text ... 
+= Prefix of sentence =
+<|endoftext|>The potato
+
+ Unperturbed generated text :
+
+<|endoftext|>The potato-shaped, potato-eating insect of modern times (Ophiocordyceps elegans) has a unique ability to adapt quickly to a wide range of environments. It is able to survive in many different environments, including the Arctic, deserts
+
+ Perturbed generated text :
+
+<|endoftext|>The potato bomb is nothing new. It's been on the news a lot since 9/11. In fact, since the bombing in Paris last November, a bomb has been detonated in every major European country in the European Union.
+
+The bomb in Brussels
+
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+From github.com:arita37/mlmodels_store
+   65684cb..d80e053  master     -> origin/master
+Updating 65684cb..d80e053
+Fast-forward
+ error_list/20200513/list_log_jupyter_20200513.md   | 2273 ++++++++++----------
+ error_list/20200513/list_log_test_cli_20200513.md  |  152 +-
+ error_list/20200513/list_log_testall_20200513.md   |  264 +--
+ ...-10_6672e19fe4cfa7df885e45d91d645534b8989485.py |  621 ++++++
+ 4 files changed, 1994 insertions(+), 1316 deletions(-)
+ create mode 100644 log_pullrequest/log_pr_2020-05-13-02-10_6672e19fe4cfa7df885e45d91d645534b8989485.py
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
