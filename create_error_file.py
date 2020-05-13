cc5794ad1a174d90e28483a3a929722582d6cb07
arita37 <noelkev0@gmail.com>
2020-05-13 19:47:32 +0900

    create_error_file.py

--- create_error_file.py
+++ create_error_file.py
@@ -36,17 +36,21 @@ def create_error_list(latest_file):
         lines = fp.readlines()  # read the lines in the file
         for idx, line in enumerate(lines):  # loop over lines
             error_found = False
+
             if 'TAG ::' in line:
                 json_acceptable_string = line.split(
                     "::")[1].strip().replace("'", "\"")
                 tag_dict = json.loads(json_acceptable_string)
+
             if 'Traceback' in line:
                 traceback_found = True
                 error_found = False
                 line = str(idx + 1) + '..' + line
+
             if error_re.match(line) is not None:
                 traceback_found = False
                 error_found = True
+
             if traceback_found or error_found:
                 output.append(line)
 
@@ -85,8 +89,8 @@ def create_error_file(
                     f"\n\n\n### Error {error_cnt}, [Traceback at line {line_of_traceback}]({latest_file_link}#L{line_of_traceback})")
                 
             ii = ""
-            if ", line" in line2 :
-               ii = line2.split(",")[1].replace("line", " ").strip()    
+            if ", line" in line :
+               ii = line.split(",")[1].replace("line", " ").strip()      ### Lin number in source code
 
             if '/home/runner/work/mlmodels/mlmodels/mlmodels/' in line:
                 line = line.replace(
@@ -96,45 +100,6 @@ def create_error_file(
 
         print(f"Sucessfully created the error file {output_file_name}")
 
-
-def log_info_repo(arg=None):
-    """
-       Grab Github Variables
-       https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
-       log_info_repo(arg=None)
-    """
-    repo = os_bash("echo $GITHUB_REPOSITORY")
-    sha = os_bash("echo $GITHUB_SHA")
-    workflow = os_bash("echo $GITHUB_WORKFLOW")
-    branch = os_bash("echo $GITHUB_REF")
-
-    repo = repo.replace("\n", "").replace("\r", "").strip()
-    workflow = workflow.replace("\n", "").replace("\r", "").strip()
-    sha = sha.replace("\n", "").replace("\r", "").strip()
-    branch = branch.replace(
-        "\n",
-        "").replace(
-        "\r",
-        "").strip().replace(
-            "refs/heads/",
-        "")
-
-    github_repo_url = f"https://github.com/{repo}/tree/{sha}"
-    url_branch_file = f"https://github.com/{repo}/blob/{branch}/"
-
-    url_branch_file2 = f"https://github.com/{repo}/tree/{branch}/"
-
-    return_dict = {
-        "github_repo_url": github_repo_url,
-        "url_branch_file": url_branch_file,
-        "repo": repo,
-        "branch": branch,
-        "sha": sha,
-        "workflow": workflow
-    }
-
-    print(return_dict)
-    return return_dict
 
 
 def main():
@@ -157,26 +122,22 @@ def main():
 
     # Main Script
     for log_folder in log_folders:
-        dir = os.path.dirname(__file__)  # File Path
-        file_path = os.path.join(dir, log_folder + '/*.py')
-        list_of_files = glob.glob(file_path)
+        dir              = os.path.dirname(__file__)  # File Path
+        file_path        = os.path.join(dir, log_folder + '/*.py')
+        list_of_files    = glob.glob(file_path)
         latest_file_path = max(list_of_files, key=os.path.getctime)
         latest_file_name = latest_file_path.split('/')[-1]
         latest_file_link = "https://github.com/{}/mlmodels_store/blob/master/{}".format(
-            USERNAME, latest_file_path.split('/')[-2] + '/' + latest_file_name)  # Original file url
+                           USERNAME, latest_file_path.split('/')[-2] + '/' + latest_file_name)  # Original file url
 
         output, tag_dict = create_error_list(latest_file_path)
 
         # In the format
         # /error_list/execution_date/list_log_folder_executionDate.md
-        execution_date = str(dt.datetime.now().date()).replace('-', '')
-        output_file_dir = os.path.join(dir,
-                                       "error_list/" +
-                                       execution_date +
-                                       '/')
+        execution_date   = str(dt.datetime.now().date()).replace('-', '')
+        output_file_dir  = os.path.join(dir, "error_list/" + execution_date + '/')
+        output_file_name = "list" +  "_{}_".format(log_folder) + execution_date + '.md'
 
-        output_file_name = "list" + \
-            "_{}_".format(log_folder) + execution_date + '.md'
         create_error_file(
             output,
             tag_dict,
@@ -187,3 +148,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
