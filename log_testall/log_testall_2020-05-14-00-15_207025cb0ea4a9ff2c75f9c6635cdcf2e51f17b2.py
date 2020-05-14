6ee1373c7372e6870808e3aa3a2271a79322e1fb
arita37 <noelkev0@gmail.com>
2020-05-14 01:28:23 +0000

    ml_store

--- log_testall/log_testall_2020-05-14-00-15_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+++ log_testall/log_testall_2020-05-14-00-15_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
@@ -10394,3 +10394,34 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master e281d47] ml_store
+ 1 file changed, 111 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   ef01be6..e281d47  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//mlp.py 
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+Already up to date.
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
