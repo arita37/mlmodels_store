ed69d35447516c473c2d6922c87a3453f9219272
arita37 <noelkev0@gmail.com>
2020-05-15 15:53:07 +0000

    ml_store

---
+++ log_import/log_import_2020-05-15-15-52_169ff9dd8baf94be9a49cc5b3e3dcd3c926c4077.py
@@ -0,0 +1,136 @@
+
+  test_import /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_import', folder=None, log_file=None, save_folder='ztest/') 
+
+  ml_test --do test_import 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/169ff9dd8baf94be9a49cc5b3e3dcd3c926c4077', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '169ff9dd8baf94be9a49cc5b3e3dcd3c926c4077', 'workflow': 'test_fast_linux'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_fast_linux
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/169ff9dd8baf94be9a49cc5b3e3dcd3c926c4077
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/169ff9dd8baf94be9a49cc5b3e3dcd3c926c4077
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ************************************************************************************************************************
+
+  test_import 
+['model_flow.__init__', 'model_keras.keras_gan', 'model_keras.textcnn_dataloader', 'model_keras.preprocess', 'model_keras.nbeats', 'model_keras.01_deepctr', 'model_keras.textvae', 'model_keras.namentity_crm_bilstm_dataloader', 'model_keras.Autokeras', 'model_keras.util', 'model_keras.charcnn_zhang', 'model_keras.charcnn', 'model_keras.__init__', 'model_keras.namentity_crm_bilstm', 'model_keras.textcnn', 'model_keras.armdn', 'model_keras.02_cnn', 'model_dev.__init__', 'model_tf.1_lstm', 'model_tf.util', 'model_tf.__init__', 'model_tf.temporal_fusion_google', 'model_gluon.util', 'model_gluon.gluon_automl', 'model_gluon.util_autogluon', 'model_gluon.fb_prophet', 'model_gluon.__init__', 'model_gluon.gluonts_model', 'model_sklearn.model_sklearn', 'model_sklearn.model_lightgbm', 'model_sklearn.__init__', 'example.vision_mnist', 'example.benchmark_timeseries_m4', 'example.arun_hyper', 'example.lightgbm_glass', 'example.benchmark_timeseries_m5', 'example.arun_model', 'utils.ztest_structure', 'utils.test_dataloader', 'utils.parse', 'model_tch.util_transformer', 'model_tch.nbeats', 'model_tch.transformer_classifier', 'model_tch.matchzoo_models', 'model_tch.util_data', 'model_tch.torchhub', 'model_tch.03_nbeats_dataloader', 'model_tch.__init__', 'model_tch.transformer_sentence', 'model_tch.pytorch_vae', 'model_tch.pplm', 'model_tch.textcnn', 'model_tch.mlp', 'template.model_xxx', 'template.00_template_keras', 'preprocess.tabular_keras', 'preprocess.image', 'preprocess.timeseries', 'preprocess.ztemp', 'preprocess.text_torch', 'preprocess.text_keras', 'preprocess.text', 'preprocess.tabular', 'preprocess.generic_old', 'preprocess.__init__', 'preprocess.generic', 'model_rank.__init__']
+mlmodels.model_flow.__init__
+
+  Error mlmodels.model_keras.keras_gan module 'mlmodels.model_keras.raw.keras_gan' has no attribute 'aae' 
+mlmodels.model_keras.textcnn_dataloader
+mlmodels.model_keras.preprocess
+mlmodels.model_keras.nbeats
+mlmodels.model_keras.01_deepctr
+mlmodels.model_keras.textvae
+mlmodels.model_keras.namentity_crm_bilstm_dataloader
+
+  Error mlmodels.model_keras.Autokeras No module named 'autokeras' 
+
+  Error mlmodels.model_keras.util create_model() takes exactly 1 positional argument (0 given) 
+Using TensorFlow backend.
+WARNING:tensorflow:From /opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset
+mlmodels.model_keras.charcnn_zhang
+mlmodels.model_keras.charcnn
+mlmodels.model_keras.__init__
+mlmodels.model_keras.namentity_crm_bilstm
+mlmodels.model_keras.textcnn
+mlmodels.model_keras.armdn
+mlmodels.model_keras.02_cnn
+mlmodels.model_dev.__init__
+mlmodels.model_tf.1_lstm
+mlmodels.model_tf.util
+mlmodels.model_tf.__init__
+
+  Error mlmodels.model_tf.temporal_fusion_google No module named 'mlmodels.mode_tf' 
+
+  Error mlmodels.model_gluon.util create_model() takes exactly 1 positional argument (0 given) 
+mlmodels.model_gluon.gluon_automl
+mlmodels.model_gluon.util_autogluon
+mlmodels.model_gluon.fb_prophet
+mlmodels.model_gluon.__init__
+
+  Error mlmodels.model_gluon.gluonts_model create_model() takes exactly 1 positional argument (0 given) 
+mlmodels.model_sklearn.model_sklearn
+mlmodels.model_sklearn.model_lightgbm
+mlmodels.model_sklearn.__init__
+
+  Error mlmodels.example.vision_mnist invalid syntax (vision_mnist.py, line 15) 
+mlmodels.example.benchmark_timeseries_m4
+
+  Error mlmodels.example.arun_hyper name 'mlmodels' is not defined 
+Deprecaton set to False
+/home/runner/work/mlmodels/mlmodels
+
+  Error mlmodels.example.lightgbm_glass [Errno 2] No such file or directory: 'lightgbm_glass.json' 
+
+  Error mlmodels.example.benchmark_timeseries_m5 [Errno 2] File b'./m5-forecasting-accuracy/calendar.csv' does not exist: b'./m5-forecasting-accuracy/calendar.csv' 
+<module 'mlmodels' from '/home/runner/work/mlmodels/mlmodels/mlmodels/__init__.py'>
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json
+
+  Error mlmodels.example.arun_model [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json' 
+mlmodels.utils.ztest_structure
+mlmodels.utils.test_dataloader
+mlmodels.utils.parse
+mlmodels.model_tch.util_transformer
+mlmodels.model_tch.nbeats
+
+  Error mlmodels.model_tch.transformer_classifier No module named 'util_transformer' 
+mlmodels.model_tch.matchzoo_models
+
+  Error mlmodels.model_tch.util_data [Errno 2] File b'./data/train.csv' does not exist: b'./data/train.csv' 
+mlmodels.model_tch.torchhub
+
+  Error mlmodels.model_tch.03_nbeats_dataloader No module named 'dataloader' 
+/opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/site-packages/mxnet/optimizer/optimizer.py:167: UserWarning: WARNING: New optimizer gluonnlp.optimizer.lamb.LAMB is overriding existing optimizer mxnet.optimizer.optimizer.LAMB
+  Optimizer.opt_registry[name].__name__))
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon/gluonts_model.py:569: DeprecationWarning:
+
+invalid escape sequence \s
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_sklearn/model_sklearn.py:1187: DeprecationWarning:
+
+invalid escape sequence \*
+
+PyTorch version 1.2.0 available.
+mlmodels.model_tch.__init__
+mlmodels.model_tch.transformer_sentence
+
+  Error mlmodels.model_tchtorch_vae No module named 'mlmodels.model_tchtorch_vae' 
+mlmodels.model_tch.pplm
+mlmodels.model_tch.textcnn
+mlmodels.model_tch.mlp
+
+  Error mlmodels.template.model_xxx name '__file___' is not defined 
+
+  Error mlmodels.template.00_template_keras expected an indented block (00_template_keras.py, line 68) 
+mlmodels.preprocess.tabular_keras
+mlmodels.preprocess.image
+mlmodels.preprocess.timeseries
+
+  Error mlmodels.preprocess.ztemp invalid character in identifier (ztemp.py, line 6) 
+mlmodels.preprocess.text_torch
+mlmodels.preprocess.text_keras
+mlmodels.preprocess.text
+mlmodels.preprocess.tabular
+mlmodels.preprocess.generic_old
+mlmodels.preprocess.__init__
+mlmodels.preprocess.generic
+mlmodels.model_rank.__init__
