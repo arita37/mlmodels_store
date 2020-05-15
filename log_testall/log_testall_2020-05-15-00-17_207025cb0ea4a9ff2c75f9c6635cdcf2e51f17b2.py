8abe36950c6dd80b508dbf9420017fa420cd5c79
arita37 <noelkev0@gmail.com>
2020-05-15 01:27:50 +0000

    ml_store

--- log_testall/log_testall_2020-05-15-00-17_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+++ log_testall/log_testall_2020-05-15-00-17_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
@@ -11002,3 +11002,59 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 4671b58] ml_store
+ 1 file changed, 35 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   a70b154..4671b58  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py 
+
+  #### Loading params   ############################################## 
+
+  #### Path params   ########################################## 
+
+  #### Loading dataset   ############################################# 
+
+  #### Model init, fit   ############################################# 
+Epoch:   0%|          | 0/1 [00:00<?, ?it/s]
+Iteration:   0%|          | 0/29440 [00:00<?, ?it/s][A
+Iteration:   0%|          | 1/29440 [00:15<127:19:37, 15.57s/it][A
+Iteration:   0%|          | 2/29440 [01:45<310:41:43, 38.00s/it][A
+Iteration:   0%|          | 3/29440 [02:29<324:24:20, 39.67s/it][A
+Iteration:   0%|          | 4/29440 [04:54<583:40:58, 71.38s/it][A
+Iteration:   0%|          | 5/29440 [06:21<621:13:17, 75.98s/it][AKilled
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+From github.com:arita37/mlmodels_store
+   4671b58..937d3cb  master     -> origin/master
+Updating 4671b58..937d3cb
+Fast-forward
+ error_list/20200515/list_log_benchmark_20200515.md |  182 ++-
+ .../20200515/list_log_dataloader_20200515.md       |    2 +-
+ error_list/20200515/list_log_jupyter_20200515.md   | 1686 ++++++++++----------
+ .../20200515/list_log_pullrequest_20200515.md      |    2 +-
+ ...-10_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py |  611 +++++++
+ 5 files changed, 1563 insertions(+), 920 deletions(-)
+ create mode 100644 log_pullrequest/log_pr_2020-05-15-01-10_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
