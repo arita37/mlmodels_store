a57d4f393d44e736477ddc9efeb3767bb3c5dee6
arita37 <noelkev0@gmail.com>
2020-05-15 21:40:51 +0000

    ml_store

--- log_testall/log_testall_2020-05-15-20-11_d580c5017e28eefaf82dbb63ddf4270e71792c2b.py
+++ log_testall/log_testall_2020-05-15-20-11_d580c5017e28eefaf82dbb63ddf4270e71792c2b.py
@@ -11130,3 +11130,59 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master f672d85] ml_store
+ 1 file changed, 35 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   38bd599..f672d85  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//pplm.py 
+ Generating text ... 
+= Prefix of sentence =
+<|endoftext|>The potato
+
+ Unperturbed generated text :
+
+<|endoftext|>The potato-shaped, potato-eating insect of modern times (Ophiocordyceps elegans) has a unique ability to adapt quickly to a wide range of environments. It is able to survive in many different environments, including the Arctic, deserts
+
+ Perturbed generated text :
+
+<|endoftext|>The potato bomb is nothing new. It's been on the news a lot since 9/11. In fact, since the bombing in Paris last November, a bomb has been detonated in every major European country in the European Union.
+
+The bomb in Brussels
+
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+From github.com:arita37/mlmodels_store
+   f672d85..b000c6f  master     -> origin/master
+Updating f672d85..b000c6f
+Fast-forward
+ .../20200515/list_log_dataloader_20200515.md       |    2 +-
+ error_list/20200515/list_log_import_20200515.md    |    2 +-
+ error_list/20200515/list_log_json_20200515.md      | 1146 ++++++-------
+ error_list/20200515/list_log_jupyter_20200515.md   | 1749 ++++++++++----------
+ .../20200515/list_log_pullrequest_20200515.md      |    2 +-
+ error_list/20200515/list_log_test_cli_20200515.md  |  378 +++--
+ error_list/20200515/list_log_testall_20200515.md   |    7 +
+ 7 files changed, 1652 insertions(+), 1634 deletions(-)
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
