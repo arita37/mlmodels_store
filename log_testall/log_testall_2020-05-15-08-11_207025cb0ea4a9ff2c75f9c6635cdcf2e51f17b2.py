d7fbf4ada159f7741c9b84e3181e54282c2a3762
arita37 <noelkev0@gmail.com>
2020-05-15 08:12:19 +0000

    ml_store

--- log_testall/log_testall_2020-05-15-08-11_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+++ log_testall/log_testall_2020-05-15-08-11_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
@@ -104,3 +104,61 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 5072c69] ml_store
+ 1 file changed, 47 insertions(+)
+To github.com:arita37/mlmodels_store.git
+ ! [rejected]        master -> master (fetch first)
+error: failed to push some refs to 'git@github.com:arita37/mlmodels_store.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//nbeats.py 
+
+  #### Loading params   ############################################## 
+
+  #### Path params   ########################################## 
+
+  #### Loading dataset   ############################################# 
+Using TensorFlow backend.
+Loading data...
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//nbeats.py", line 315, in <module>
+    test(pars_choice="test01")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//nbeats.py", line 278, in test
+    Xtuple = get_dataset(data_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//nbeats.py", line 172, in get_dataset
+    train_data = Data(data_source= path_norm( data_pars["train_data_source"]) ,
+NameError: name 'Data' is not defined
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all    ;            ls &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Fetching origin
+From github.com:arita37/mlmodels_store
+   2684e18..74d1c56  master     -> origin/master
+Merge made by the 'recursive' strategy.
+ ...-10_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py | 628 +++++++++++++++++++++
+ 1 file changed, 628 insertions(+)
+ create mode 100644 log_pullrequest/log_pr_2020-05-15-08-10_207025cb0ea4a9ff2c75f9c6635cdcf2e51f17b2.py
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
