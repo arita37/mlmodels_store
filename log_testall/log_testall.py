de236dd76daf458d116eab15b3f9ad6295450d02
arita37 <noelkev0@gmail.com>
2020-05-16 22:40:23 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -11819,3 +11819,35 @@ Fast-forward
  error_list/20200516/list_log_test_cli_20200516.md |  364 ++---
  error_list/20200516/list_log_testall_20200516.md  |  386 ++---
  4 files changed, 1804 insertions(+), 1842 deletions(-)
+[master f1874fc] ml_store
+ 2 files changed, 114 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   630315e..f1874fc  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//mlp.py 
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Already up to date.
