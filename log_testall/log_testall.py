1bb60b503440c3e0b0a6f006702c683783ace926
arita37 <noelkev0@gmail.com>
2020-05-17 16:32:53 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -8285,3 +8285,39 @@ Updating 113f5cf..f6ef417
 Fast-forward
  error_list/20200517/list_log_testall_20200517.md | 386 ++++++++++-------------
  1 file changed, 173 insertions(+), 213 deletions(-)
+[master 1f8eb7e] ml_store
+ 1 file changed, 204 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   f6ef417..1f8eb7e  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon//fb_prophet.py 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon//fb_prophet.py", line 160, in <module>
+    test(data_path = "model_fb/fbprophet.json", choice="json" )
+TypeError: test() got an unexpected keyword argument 'choice'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Already up to date.
