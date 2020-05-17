f7807423a318798e82ab0192821acc62a6ba4f45
arita37 <noelkev0@gmail.com>
2020-05-17 05:16:02 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -10735,3 +10735,39 @@ log_testall
 test_jupyter
 Fetching origin
 Already up to date.
+[master 525efb8] ml_store
+ 1 file changed, 149 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   7f596be..525efb8  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//03_nbeats_dataloader.py 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//03_nbeats_dataloader.py", line 9, in <module>
+    from dataloader import DataLoader
+ModuleNotFoundError: No module named 'dataloader'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Already up to date.
