30d2aea9355c64ec420e60e61522d94e4d12cdb7
arita37 <noelkev0@gmail.com>
2020-05-19 20:24:10 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -5163,3 +5163,40 @@ log_testall
 test_jupyter
 Fetching origin
 Already up to date.
+[master 3386708] ml_store
+ 1 file changed, 47 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   ba8eb9f..3386708  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//Autokeras.py 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//Autokeras.py", line 12, in <module>
+    import autokeras as ak
+ModuleNotFoundError: No module named 'autokeras'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Warning: Permanently added the RSA host key for IP address '140.82.114.4' to the list of known hosts.
+Already up to date.
