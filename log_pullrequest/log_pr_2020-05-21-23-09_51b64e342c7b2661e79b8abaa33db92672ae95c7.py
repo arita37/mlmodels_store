335986cde47d117e30245edad016eb76f6c42f15
arita37 <noelkev0@gmail.com>
2020-05-21 23:10:38 +0000

    ml_store

---
+++ log_pullrequest/log_pr_2020-05-21-23-09_51b64e342c7b2661e79b8abaa33db92672ae95c7.py
@@ -0,0 +1,62 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '51b64e342c7b2661e79b8abaa33db92672ae95c7', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '51b64e342c7b2661e79b8abaa33db92672ae95c7', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ************************************************************************************************************************
+
+  test_import 
