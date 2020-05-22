1e4627e0175358c3f5c0b714102edc2501543186
arita37 <noelkev0@gmail.com>
2020-05-22 05:04:15 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -7847,3 +7847,54 @@ Fast-forward
  .../20200522/list_log_pullrequest_20200522.md      |   2 +-
  error_list/20200522/list_log_testall_20200522.md   | 375 +++++++++++++++++++++
  2 files changed, 376 insertions(+), 1 deletion(-)
+[master 9cf2bb5] ml_store
+ 1 file changed, 4953 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   9da0864..9cf2bb5  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//textvae.py 
+
+  #### Loading params   ############################################## 
+
+  #### Path params   ################################################### 
+
+  #### Model params   ################################################# 
+
+  #### Loading dataset   ############################################# 
+Using TensorFlow backend.
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//textvae.py", line 356, in <module>
+    test(pars_choice="test01")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//textvae.py", line 327, in test
+    xtuple = get_dataset(data_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//textvae.py", line 269, in get_dataset
+    with codecs.open(data_pars["train_data_path"], encoding='utf-8') as f:
+  File "/opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/codecs.py", line 897, in open
+    file = builtins.open(filename, mode, buffering)
+FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/text/quora/train.csv'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Already up to date.
