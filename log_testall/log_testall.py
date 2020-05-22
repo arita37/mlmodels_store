c17c7a3ca1e9be67246511a40328b468758c0a05
arita37 <noelkev0@gmail.com>
2020-05-22 12:53:36 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -2661,3 +2661,51 @@ test_jupyter
 Fetching origin
 Warning: Permanently added the RSA host key for IP address '140.82.112.4' to the list of known hosts.
 Already up to date.
+[master 079a207] ml_store
+ 1 file changed, 46 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   db0098c..079a207  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py 
+
+  #### Loading params   ############################################## 
+
+  #### Path params   ########################################## 
+
+  #### Loading dataset   ############################################# 
+Using TensorFlow backend.
+Loading data...
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 357, in <module>
+    test(pars_choice="test01")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 320, in test
+    Xtuple = get_dataset(data_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 216, in get_dataset
+    if data_pars['type'] == "npz":
+KeyError: 'type'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git pull --all  &&  git add --all &&  git commit -m "ml_store"  ;            git push --all ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
+Fetching origin
+Already up to date.
