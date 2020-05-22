206348f1df2594f30c4304f915b1461299178111
arita37 <noelkev0@gmail.com>
2020-05-22 05:10:56 +0000

    ml_store

---
+++ log_pullrequest/log_pr_2020-05-22-05-10_51b64e342c7b2661e79b8abaa33db92672ae95c7.py
@@ -0,0 +1,75 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '51b64e342c7b2661e79b8abaa33db92672ae95c7', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '51b64e342c7b2661e79b8abaa33db92672ae95c7', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/51b64e342c7b2661e79b8abaa33db92672ae95c7
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ************************************************************************************************************************
+
+  test_import 
+['example.arun_model', 'example.lightgbm_glass', 'example.arun_hyper', 'example.vision_mnist', 'example.benchmark_timeseries_m5', 'example.benchmark_timeseries_m4', 'model_tf.__init__', 'model_tf.temporal_fusion_google', 'model_tf.1_lstm', 'model_tf.util', 'template.model_xxx', 'template.00_template_keras', 'preprocess.__init__', 'preprocess.text_keras', 'preprocess.image', 'preprocess.text', 'preprocess.text_torch', 'preprocess.timeseries', 'preprocess.tabular', 'preprocess.ztemp', 'preprocess.generic', 'preprocess.generic_old', 'preprocess.tabular_keras', 'model_flow.__init__', 'model_tch.matchzoo_models', 'model_tch.__init__', 'model_tch.util_data', 'model_tch.pplm', 'model_tch.util_transformer', 'model_tch.nbeats', 'model_tch.pytorch_vae', 'model_tch.03_nbeats_dataloader', 'model_tch.textcnn', 'model_tch.torchhub', 'model_tch.transformer_classifier', 'model_tch.transformer_sentence', 'model_tch.mlp', 'model_sklearn.__init__', 'model_sklearn.model_lightgbm', 'model_sklearn.model_sklearn', 'model_rank.__init__', 'utils.test_dataloader', 'utils.parse', 'utils.ztest_structure', 'model_dev.__init__', 'model_gluon.__init__', 'model_gluon.gluonts_model', 'model_gluon.fb_prophet', 'model_gluon.util_autogluon', 'model_gluon.util', 'model_gluon.gluon_automl', 'model_keras.__init__', 'model_keras.namentity_crm_bilstm_dataloader', 'model_keras.charcnn', 'model_keras.02_cnn', 'model_keras.armdn', 'model_keras.textvae', 'model_keras.01_deepctr', 'model_keras.nbeats', 'model_keras.util', 'model_keras.textcnn', 'model_keras.preprocess', 'model_keras.keras_gan', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_keras.namentity_crm_bilstm']
+<module 'mlmodels' from '/home/runner/work/mlmodels/mlmodels/mlmodels/__init__.py'>
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json
+
+  Error mlmodels.example.arun_model [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json' 
+Deprecaton set to False
+/home/runner/work/mlmodels/mlmodels
+
+  Error mlmodels.example.lightgbm_glass [Errno 2] No such file or directory: 'lightgbm_glass.json' 
+
+  Error mlmodels.example.arun_hyper name 'mlmodels' is not defined 
+
+  Error mlmodels.example.vision_mnist invalid syntax (vision_mnist.py, line 15) 
