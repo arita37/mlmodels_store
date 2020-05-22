473f7789cab201db2a8787645015b797288f5ae0
arita37 <noelkev0@gmail.com>
2020-05-22 15:11:09 +0000

    ml_store

---
+++ log_pullrequest/log_pr_2020-05-22-15-10_a463a24ea257f46bfcbd4006f805952aace8f2b1.py
@@ -0,0 +1,72 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'a463a24ea257f46bfcbd4006f805952aace8f2b1', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'a463a24ea257f46bfcbd4006f805952aace8f2b1', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/a463a24ea257f46bfcbd4006f805952aace8f2b1
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ************************************************************************************************************************
+
+  test_import 
+WARNING:tensorflow:From /opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+['model_rank.__init__', 'model_flow.__init__', 'model_tf.util', 'model_tf.1_lstm', 'model_tf.temporal_fusion_google', 'model_tf.__init__', 'example.benchmark_timeseries_m5', 'example.arun_model', 'example.arun_hyper', 'example.lightgbm_glass', 'example.benchmark_timeseries_m4', 'example.vision_mnist', 'utils.ztest_structure', 'utils.parse', 'utils.test_dataloader', 'model_gluon.util', 'model_gluon.util_autogluon', 'model_gluon.gluonts_model', 'model_gluon.__init__', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'preprocess.tabular_keras', 'preprocess.image', 'preprocess.ztemp', 'preprocess.text_torch', 'preprocess.tabular', 'preprocess.text_keras', 'preprocess.generic_old', 'preprocess.text', 'preprocess.__init__', 'preprocess.generic', 'preprocess.timeseries', 'model_tch.03_nbeats_dataloader', 'model_tch.util_data', 'model_tch.transformer_sentence', 'model_tch.util_transformer', 'model_tch.pytorch_vae', 'model_tch.mlp', 'model_tch.torchhub', 'model_tch.nbeats', 'model_tch.__init__', 'model_tch.matchzoo_models', 'model_tch.transformer_classifier', 'model_tch.textcnn', 'model_tch.pplm', 'model_sklearn.model_sklearn', 'model_sklearn.__init__', 'model_sklearn.model_lightgbm', 'template.model_xxx', 'template.00_template_keras', 'model_keras.util', 'model_keras.keras_gan', 'model_keras.02_cnn', 'model_keras.01_deepctr', 'model_keras.armdn', 'model_keras.charcnn', 'model_keras.namentity_crm_bilstm_dataloader', 'model_keras.textvae', 'model_keras.nbeats', 'model_keras.charcnn_zhang', 'model_keras.__init__', 'model_keras.textcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.preprocess', 'model_keras.Autokeras', 'model_dev.__init__']
+mlmodels.model_rank.__init__
+mlmodels.model_flow.__init__
+mlmodels.model_tf.util
+mlmodels.model_tf.1_lstm
+
+  Error mlmodels.model_tf.temporal_fusion_google No module named 'mlmodels.mode_tf' 
