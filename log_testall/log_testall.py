6d292d4f960bea92011b19736201ebeb7bc43d09
arita37 <noelkev0@gmail.com>
2020-05-25 09:17:25 +0000

    ml_store  && git pull --all

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -9849,3 +9849,33 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master d5d82e6] ml_store  && git pull --all
+ 2 files changed, 107 insertions(+)
+To github.com:arita37/mlmodels_store.git
+ + a406928...d5d82e6 master -> master (forced update)
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//mlp.py 
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git add --all &&  git commit -m "ml_store  && git pull --all"  ;            git push --all -f ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
