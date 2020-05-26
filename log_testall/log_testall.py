804c5429486c18fe21221b79595d07d7ed796f96
arita37 <noelkev0@gmail.com>
2020-05-26 09:28:49 +0000

    ml_store  && git pull --all

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -10733,3 +10733,7 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master eb9dadc] ml_store  && git pull --all
+ 1 file changed, 30 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   73526cf..eb9dadc  master -> master
