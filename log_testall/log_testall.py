5e863bc546935911d5e127f09368c3cfac5743b1
arita37 <noelkev0@gmail.com>
2020-05-27 20:26:22 +0000

    ml_store  && git pull --all

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -5146,3 +5146,37 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master e49890e] ml_store  && git pull --all
+ 1 file changed, 45 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   b30052a..e49890e  master -> master
+
+
+
+
+
+ ************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//Autokeras.py 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//Autokeras.py", line 12, in <module>
+    import autokeras as ak
+ModuleNotFoundError: No module named 'autokeras'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git add --all &&  git commit -m "ml_store  && git pull --all"  ;            git push --all -f ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
