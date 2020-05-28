37e4d7acd3ca097fd7ab4fa0882da7728af5586d
arita37 <noelkev0@gmail.com>
2020-05-28 12:56:12 +0000

    ml_store

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -1605,3 +1605,55 @@ log_pullrequest
 log_test_cli
 log_testall
 test_jupyter
+[master 4910401] ml_store
+ 1 file changed, 101 insertions(+)
+To github.com:arita37/mlmodels_store.git
+   2de214a..4910401  master -> master
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py 
+
+  #### Loading params   ############################################## 
+
+  #### Loading dataset   ############################################# 
+
+  #### Model init, fit   ############################################# 
+
+  ############ Dataloader setup  ############################# 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 488, in <module>
+    test(pars_choice="json", data_path="model_tch/transformer_sentence.json", config_mode="test")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 448, in test
+    model, session = fit(model, data_pars, model_pars, compute_pars, out_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 139, in fit
+    train_data       = SentencesDataset(train_reader.get_examples(train_fname),  model=model.model)
+  File "/opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/site-packages/sentence_transformers/readers/NLIDataReader.py", line 21, in get_examples
+    mode="rt", encoding="utf-8").readlines()
+  File "/opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/gzip.py", line 53, in open
+    binary_file = GzipFile(filename, gz_mode, compresslevel)
+  File "/opt/hostedtoolcache/Python/3.6.10/x64/lib/python3.6/gzip.py", line 163, in __init__
+    fileobj = self.myfileobj = builtins.open(filename, mode or 'rb')
+FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/text/AllNLI/s1.train.gz'
+
+   cd /home/runner/work/mlmodels/mlmodels_store/ ;            pip3 freeze > deps.txt ;            ls ;            git config --local user.email "noelkev0@gmail.com" && git config --local user.name "arita37"         ;            git add --all &&  git commit -m "ml_store"    ;            git push --all -f ;            cd /home/runner/work/mlmodels/mlmodels/ ;         
+Logs
+README.md
+README_actions.md
+create_error_file.py
+create_github_issues.py
+deps.txt
+error_list
+log_benchmark
+log_dataloader
+log_import
+log_json
+log_jupyter
+log_pullrequest
+log_test_cli
+log_testall
+test_jupyter
