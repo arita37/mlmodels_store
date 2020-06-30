554e5e408965a482685db0c7d2b5d68c790ce0b8
arita37 <noelkev0@gmail.com>
2020-06-30 10:12:42 +0000

    m_test_all.yml

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -78,7 +78,7 @@ RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
 
   #### Model init   ############################################ 
 
-  <mlmodels.model_sklearn.model_sklearn.Model object at 0x7f40e0db3240> 
+  <mlmodels.model_sklearn.model_sklearn.Model object at 0x7fa51fa2c240> 
 
   #### Fit   ######################################################## 
 
