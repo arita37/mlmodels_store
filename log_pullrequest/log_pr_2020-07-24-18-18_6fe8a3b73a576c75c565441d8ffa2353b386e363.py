803f4426a24391ad27290fb0f26812090f274fe1
arita37 <noelkev0@gmail.com>
2020-07-24 18:19:24 +0000

    m_pullrequest

---
+++ log_pullrequest/log_pr_2020-07-24-18-18_6fe8a3b73a576c75c565441d8ffa2353b386e363.py
@@ -0,0 +1,70 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, name='ml_store', save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '6fe8a3b73a576c75c565441d8ffa2353b386e363', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '6fe8a3b73a576c75c565441d8ffa2353b386e363', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/6fe8a3b73a576c75c565441d8ffa2353b386e363
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  test_import 
+['model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_keras.armdn', 'model_keras.util', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.preprocess', 'model_keras.__init__', 'model_rank.__init__', 'model_sklearn.model_lightgbm', 'model_sklearn.__init__', 'model_sklearn.model_sklearn', 'utils.parse', 'utils.ztest_structure', 'utils.test_dataloader', 'model_gluon.util_autogluon', 'model_gluon.fb_prophet', 'model_gluon.util', 'model_gluon.gluon_automl', 'model_gluon.__init__', 'model_gluon.gluonts_model_old', 'model_gluon.gluonts_model', 'example.arun_hyper', 'example.benchmark_timeseries_m5', 'example.lightgbm_glass', 'example.vision_mnist', 'example.arun_model', 'example.benchmark_timeseries_m4', 'model_tch.util_data', 'model_tch.matchZoo', 'model_tch.torchhub', 'model_tch.util_transformer', 'model_tch.textcnn', 'model_tch.__init__', 'model_tch.transformer_sentence', 'model_tf.1_lstm', 'model_tf.util', 'model_tf.__init__', 'template.00_template_keras', 'template.model_xxx', 'preprocess.text_torch', 'preprocess.tabular_keras', 'preprocess.generic', 'preprocess.tabular', 'preprocess.text', 'preprocess.generic_old', 'preprocess.__init__', 'preprocess.image', 'preprocess.ztemp', 'preprocess.text_keras', 'preprocess.timeseries', 'model_dev.temporal_fusion_google', 'model_dev.__init__']
+mlmodels.model_keras.charcnn
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset
+mlmodels.model_keras.charcnn_zhang
+
+  Error mlmodels.model_keras.Autokeras No module named 'autokeras' 
+
+  Error mlmodels.model_keras.armdn invalid syntax (data.py, line 126) 
