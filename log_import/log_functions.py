5cb2cf2a7dbed441a93b0c35bb75d1e413415a20
arita37 <noelkev0@gmail.com>
2020-07-26 17:34:47 +0000

    m_test_fast_linux

--- log_import/log_functions.py
+++ log_import/log_functions.py
@@ -13,7 +13,7 @@
 
  #################### {'uri': 'mlmodels.util:log', 'args': ['x1', 'passed'], 'kw_args': {}} 
 
-  <function log at 0x7f967ec4d7b8> 
+  <function log at 0x7ff403d557b8> 
 
   x1 passed 
 
@@ -23,15 +23,15 @@
 
  #################### {'uri': 'mlmodels.util:load_function_uri', 'args': [], 'kw_args': {'uri_name': 'mlmodels.util:log'}} 
 
-  <function load_function_uri at 0x7f967ec5a158> 
+  <function load_function_uri at 0x7ff403d65158> 
 
-  <function log at 0x7f967ec4d7b8> 
+  <function log at 0x7ff403d557b8> 
 
   
 
  #################### {'uri': 'mlmodels.util:os_package_root_path', 'args': [], 'kw_args': {}} 
 
-  <function os_package_root_path at 0x7f967ec4d950> 
+  <function os_package_root_path at 0x7ff403d55950> 
 
   /home/runner/work/mlmodels/mlmodels/mlmodels/ 
 
@@ -53,7 +53,7 @@
 
  #################### {'uri': 'mlmodels.model_tch.textcnn:analyze_datainfo_paths', 'args': [{'data_path': 'dataset/recommender/', 'dataset': 'IMDB_sample.txt', 'data_type': 'csv_dataset', 'batch_size': 64, 'train': True}], 'kw_args': {}} 
 
-  <function analyze_datainfo_paths at 0x7f95eea5d7b8> 
+  <function analyze_datainfo_paths at 0x7ff373b847b8> 
 
   ('/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/recommender/IMDB_sample.txt', '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/recommender/train/IMDB_sample.csv', '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/recommender/test/IMDB_sample.csv') 
 
@@ -61,7 +61,7 @@
 
  #################### {'uri': 'mlmodels.models:module_load', 'args': ['model_tch.torchhub.py'], 'kw_args': {}} 
 
-  <function module_load at 0x7f95eea60840> 
+  <function module_load at 0x7ff373b86840> 
 
   <module 'mlmodels.model_tch.torchhub' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/torchhub.py'> 
 
@@ -69,7 +69,7 @@
 
  #################### {'uri': 'mlmodels.models:module_load_full', 'args': ['model_tch.textcnn.py', {'model_uri': 'model_keras.textcnn.py', 'maxlen': 40, 'max_features': 5, 'embedding_dims': 50}, {'data_info': {'dataset': 'mlmodels/dataset/text/imdb', 'pass_data_pars': False, 'train': True, 'maxlen': 40, 'max_features': 5}, 'preprocessors': [{'name': 'loader', 'uri': 'mlmodels.preprocess.generic:NumpyDataset', 'args': {'numpy_loader_args': {'allow_pickle': True}, 'encoding': "'ISO-8859-1'"}}, {'name': 'imdb_process', 'uri': 'mlmodels.preprocess.text_keras:IMDBDataset', 'args': {'num_words': 5}}]}, {'engine': 'adam', 'loss': 'binary_crossentropy', 'metrics': ['accuracy'], 'batch_size': 1000, 'epochs': 1}], 'kw_args': {}} 
 
-  <function module_load_full at 0x7f95eea608c8> 
+  <function module_load_full at 0x7ff373b868c8> 
 {'model_uri': 'model_keras.textcnn.py', 'maxlen': 40, 'max_features': 5, 'embedding_dims': 50}
 
   'kernel_height' {'uri': 'mlmodels.models:module_load_full', 'args': ['model_tch.textcnn.py', {'model_uri': 'model_keras.textcnn.py', 'maxlen': 40, 'max_features': 5, 'embedding_dims': 50}, {'data_info': {'dataset': 'mlmodels/dataset/text/imdb', 'pass_data_pars': False, 'train': True, 'maxlen': 40, 'max_features': 5}, 'preprocessors': [{'name': 'loader', 'uri': 'mlmodels.preprocess.generic:NumpyDataset', 'args': {'numpy_loader_args': {'allow_pickle': True}, 'encoding': "'ISO-8859-1'"}}, {'name': 'imdb_process', 'uri': 'mlmodels.preprocess.text_keras:IMDBDataset', 'args': {'num_words': 5}}]}, {'engine': 'adam', 'loss': 'binary_crossentropy', 'metrics': ['accuracy'], 'batch_size': 1000, 'epochs': 1}], 'kw_args': {}} 
@@ -78,7 +78,7 @@
 
  #################### {'uri': 'mlmodels.util:path_norm', 'args': ['model_keras.charcnn.py'], 'kw_args': {}} 
 
-  <function path_norm at 0x7f967ec4dea0> 
+  <function path_norm at 0x7ff403d55ea0> 
 
   /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras.charcnn.py 
 
@@ -86,6 +86,6 @@
 
  #################### {'uri': 'mlmodels.util:path_norm_dict', 'args': [{'out_pars': {'checkpointdir': 'ztest/model_tch/MATCHZOO/BERT/checkpoints/', 'path': 'ztest/model_tch/MATCHZOO/BERT/'}}], 'kw_args': {}} 
 
-  <function path_norm_dict at 0x7f967ec4df28> 
+  <function path_norm_dict at 0x7ff403d55f28> 
 
   {'out_pars': {'checkpointdir': 'ztest/model_tch/MATCHZOO/BERT/checkpoints/', 'path': 'ztest/model_tch/MATCHZOO/BERT/'}} 
