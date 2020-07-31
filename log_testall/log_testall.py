abc9cd7f43c7e0fdc65fa04b7e841d7236f7122d
arita37 <noelkev0@gmail.com>
2020-07-31 05:14:54 +0000

    m_test_all.yml

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -25,9 +25,9 @@
 
   ############Check model ################################ 
 
-  ['model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_keras.armdn', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.namentity_crm_bilstm', 'model_sklearn.model_lightgbm', 'model_sklearn.model_sklearn', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.gluonts_model', 'model_tch.matchZoo', 'model_tch.torchhub', 'model_tch.textcnn', 'model_tch.transformer_sentence', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google'] 
+  ['model_keras.namentity_crm_bilstm', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.armdn', 'model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.gluonts_model', 'model_dev.temporal_fusion_google', 'model_tf.1_lstm', 'model_tch.torchhub', 'model_tch.transformer_sentence', 'model_tch.textcnn', 'model_tch.matchZoo', 'model_sklearn.model_sklearn', 'model_sklearn.model_lightgbm'] 
 
-  Used ['model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_keras.armdn', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.namentity_crm_bilstm', 'model_sklearn.model_lightgbm', 'model_sklearn.model_sklearn', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.gluonts_model', 'model_tch.matchZoo', 'model_tch.torchhub', 'model_tch.textcnn', 'model_tch.transformer_sentence', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google'] 
+  Used ['model_keras.namentity_crm_bilstm', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.armdn', 'model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.gluonts_model', 'model_dev.temporal_fusion_google', 'model_tf.1_lstm', 'model_tch.torchhub', 'model_tch.transformer_sentence', 'model_tch.textcnn', 'model_tch.matchZoo', 'model_sklearn.model_sklearn', 'model_sklearn.model_lightgbm'] 
 
 
 
@@ -35,18 +35,22 @@
 
  ********************************************************************************************************************************************
 
-  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py 
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py 
+
+  #### Module init   ############################################ 
+
+  <module 'mlmodels.model_keras.namentity_crm_bilstm' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/namentity_crm_bilstm.py'> 
 
   #### Loading params   ############################################## 
 Using TensorFlow backend.
 Traceback (most recent call last):
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 373, in <module>
-    test(pars_choice="json", data_path= f"dataset/json/refactor/charcnn.json")
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 330, in test
-    model_pars, data_pars, compute_pars, out_pars = get_params(param_pars)
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//charcnn.py", line 266, in get_params
-    cf = json.load(open(data_path, mode='r'))
-FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/json/refactor/charcnn.json'
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras//namentity_crm_bilstm.py", line 306, in <module>
+    test_module(model_uri=MODEL_URI, param_pars=param_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/models.py", line 257, in test_module
+    model_pars, data_pars, compute_pars, out_pars = module.get_params(param_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/namentity_crm_bilstm.py", line 197, in get_params
+    cf = json.load(open(data_path, mode="r"))
+FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/json/refactor/namentity_crm_bilstm_dataloader.json'
 Traceback (most recent call last):
   File "/opt/hostedtoolcache/Python/3.6.11/x64/bin/ml_test", line 11, in <module>
     load_entry_point('mlmodels', 'console_scripts', 'ml_test')()
