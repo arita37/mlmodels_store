0f16ca5ccf28f7c0b0ac18aec4c5b749de983c1e
arita37 <noelkev0@gmail.com>
2020-08-14 10:23:29 +0000

    m_test_all.yml

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -51,7 +51,7 @@
 
   #### metrics   ##################################################### 
 {'mode': 'test', 'path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/tabular/titanic_train_preprocessed.csv', 'data_type': 'pandas', 'train': True}
-{'roc_auc_score': 0.9642857142857143}
+{'roc_auc_score': 1.0}
 
   #### Plot   ######################################################## 
 
@@ -78,7 +78,7 @@ RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
 
   #### Model init   ############################################ 
 
-  <mlmodels.model_sklearn.model_sklearn.Model object at 0x7f9f1dbee828> 
+  <mlmodels.model_sklearn.model_sklearn.Model object at 0x7fc6fb3b3e80> 
 
   #### Fit   ######################################################## 
 
