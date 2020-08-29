d0db115ee95042b5fe621d4c526db2aaf3912d0a
arita37 <noelkev0@gmail.com>
2020-08-29 14:21:44 +0000

    m_test_fast_linux

--- log_import/log_optim.py
+++ log_import/log_optim.py
@@ -5,25 +5,23 @@ Deprecaton set to False
   <module 'mlmodels.model_tf.1_lstm' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_tf/1_lstm.py'> 
 
   ###### Hyper-optimization through study   ################################## 
-
-  check <module 'mlmodels.model_tf.1_lstm' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_tf/1_lstm.py'> {'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year_small.csv', 'data_type': 'pandas', 'size': [0, 0, 6], 'output_size': [0, 6]} 
-[W 2020-08-29 14:10:11,928] Setting status of trial#0 as TrialState.FAIL because of the following error: TypeError("fit() got multiple values for argument 'data_pars'",)
+[W 2020-08-29 14:21:34,120] Setting status of trial#0 as TrialState.FAIL because of the following error: UnboundLocalError("local variable 'module' referenced before assignment",)
 Traceback (most recent call last):
   File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
     result = func(trial)
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 146, in objective
-    model, sess = module.fit(model, data_pars=data_pars, compute_pars=compute_pars, out_pars=out_pars)
-TypeError: fit() got multiple values for argument 'data_pars'
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
 Traceback (most recent call last):
   File "/opt/hostedtoolcache/Python/3.6.11/x64/bin/ml_optim", line 11, in <module>
     load_entry_point('mlmodels', 'console_scripts', 'ml_optim')()
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 384, in main
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 382, in main
     test_json( path_json="template/optim_config_prune.json", config_mode= arg.config_mode )
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 232, in test_json
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 230, in test_json
     out_pars        = out_pars
   File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 57, in optim
     out_pars)
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 172, in optim_optuna
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 170, in optim_optuna
     study.optimize(objective, n_trials=ntrials)  # Invoke optimization of the objective function.
   File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 302, in optimize
     gc_after_trial, None)
@@ -33,6 +31,6 @@ Traceback (most recent call last):
     trial = self._run_trial(func, catch, gc_after_trial)
   File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
     result = func(trial)
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 146, in objective
-    model, sess = module.fit(model, data_pars=data_pars, compute_pars=compute_pars, out_pars=out_pars)
-TypeError: fit() got multiple values for argument 'data_pars'
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
