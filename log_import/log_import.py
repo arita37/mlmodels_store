a43ef581df9c0beb9f1e26d00d0bd91775422c1e
arita37 <noelkev0@gmail.com>
2020-08-29 05:40:00 +0000

    m_test_fast_linux

--- log_import/log_import.py
+++ log_import/log_import.py
@@ -9,17 +9,17 @@
 
  ********************************************************************************************************************************************
 
- ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/e7782fe6222147ff886292aafe98fb3f6ae70a9f', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'e7782fe6222147ff886292aafe98fb3f6ae70a9f', 'workflow': 'test_fast_linux'}
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/3bfc87b6ea811b257c8e8857136643e80047143d', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '3bfc87b6ea811b257c8e8857136643e80047143d', 'workflow': 'test_fast_linux'}
 
  ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_fast_linux
 
  ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
 
- ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/e7782fe6222147ff886292aafe98fb3f6ae70a9f
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/3bfc87b6ea811b257c8e8857136643e80047143d
 
- ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/e7782fe6222147ff886292aafe98fb3f6ae70a9f
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/3bfc87b6ea811b257c8e8857136643e80047143d
 
- ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/e7782fe6222147ff886292aafe98fb3f6ae70a9f
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/3bfc87b6ea811b257c8e8857136643e80047143d
 
  ************************************************************************************************************************
 
