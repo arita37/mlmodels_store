999b5da88624245dd3ba50c0858cd4ea4fab3b91
arita37 <noelkev0@gmail.com>
2020-08-31 18:22:17 +0000

    m_pullrequest

---
+++ log_pullrequest/log_pr_2020-08-31-18-21_76c59fd9a4bb974b18235d07ef6a03bf2361dc5c.py
@@ -0,0 +1,292 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, name='ml_store', save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '76c59fd9a4bb974b18235d07ef6a03bf2361dc5c', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '76c59fd9a4bb974b18235d07ef6a03bf2361dc5c', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/76c59fd9a4bb974b18235d07ef6a03bf2361dc5c
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  test_import 
+['model_rank.__init__', 'utils.ztest_structure', 'utils.test_dataloader', 'utils.parse', 'model_sklearn.model_sklearn', 'model_sklearn.model_lightgbm', 'model_sklearn.__init__', 'model_tf.util', 'model_tf.__init__', 'model_tf.1_lstm', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.util', 'model_keras.armdn', 'model_keras.namentity_crm_bilstm', 'model_keras.__init__', 'model_keras.preprocess', 'model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'template.00_template_keras', 'template.model_xxx', 'preprocess.text_keras', 'preprocess.tabular_keras', 'preprocess.image', 'preprocess.__init__', 'preprocess.timeseries', 'preprocess.text_torch', 'preprocess.generic_old', 'preprocess.text', 'preprocess.generic', 'preprocess.tabular', 'preprocess.ztemp', 'example.benchmark_timeseries_m5', 'example.lightgbm_glass', 'example.benchmark_timeseries_m4', 'example.vision_mnist', 'example.arun_model', 'example.arun_hyper', 'model_dev.temporal_fusion_google', 'model_dev.__init__', 'model_gluon.gluonts_model_old', 'model_gluon.util', 'model_gluon.__init__', 'model_gluon.util_autogluon', 'model_gluon.fb_prophet', 'model_gluon.gluonts_model', 'model_gluon.gluon_automl', 'model_tch.util_data', 'model_tch.matchZoo', 'model_tch.util_transformer', 'model_tch.textcnn', 'model_tch.torchhub', 'model_tch.__init__', 'model_tch.transformer_sentence']
+mlmodels.model_rank.__init__
+mlmodels.utils.ztest_structure
+mlmodels.utils.test_dataloader
+mlmodels.utils.parse
+mlmodels.model_sklearn.model_sklearn
+
+  Error mlmodels.model_sklearn.model_lightgbm invalid syntax (model_lightgbm.py, line 316) 
+Using TensorFlow backend.
+WARNING:tensorflow:From /opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+mlmodels.model_sklearn.__init__
+mlmodels.model_tf.util
+mlmodels.model_tf.__init__
+mlmodels.model_tf.1_lstm
+
+  Error mlmodels.model_keras.deepctr cannot import name 'create_embedding_matrix' 
+mlmodels.model_keras.textcnn
+mlmodels.model_keras.util
+
+  Error mlmodels.model_keras.armdn invalid syntax (data.py, line 126) 
+mlmodels.model_keras.namentity_crm_bilstm
+mlmodels.model_keras.__init__
+
+  Error mlmodels.model_keras.preprocess cannot import name 'create_embedding_matrix' 
+mlmodels.model_keras.charcnn
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset
+mlmodels.model_keras.charcnn_zhang
+
+  Error mlmodels.model_keras.Autokeras No module named 'autokeras' 
+
+  Error mlmodels.template.00_template_keras expected an indented block (00_template_keras.py, line 68) 
+
+  Error mlmodels.template.model_xxx invalid syntax (data.py, line 126) 
+mlmodels.preprocess.text_keras
+
+  Error mlmodels.preprocess.tabular_keras cannot import name 'create_embedding_matrix' 
+mlmodels.preprocess.image
+mlmodels.preprocess.__init__
+mlmodels.preprocess.timeseries
+
+  Error mlmodels.preprocess.text_torch libtorch_cpu.so: cannot open shared object file: No such file or directory 
+mlmodels.preprocess.generic_old
+mlmodels.preprocess.text
+mlmodels.preprocess.generic
+mlmodels.preprocess.tabular
+
+  Error mlmodels.preprocess.ztemp invalid character in identifier (ztemp.py, line 6) 
+
+  Error mlmodels.example.benchmark_timeseries_m5 [Errno 2] File b'./m5-forecasting-accuracy/calendar.csv' does not exist: b'./m5-forecasting-accuracy/calendar.csv' 
+Deprecaton set to False
+/home/runner/work/mlmodels/mlmodels
+
+  Error mlmodels.example.lightgbm_glass [Errno 2] No such file or directory: 'lightgbm_glass.json' 
+mlmodels.example.benchmark_timeseries_m4
+
+  Error mlmodels.example.vision_mnist invalid syntax (vision_mnist.py, line 15) 
+<module 'mlmodels' from '/home/runner/work/mlmodels/mlmodels/mlmodels/__init__.py'>
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json
+
+  Error mlmodels.example.arun_model [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/ardmn.json' 
+
+  Error mlmodels.example.arun_hyper name 'copy' is not defined 
+
+  Error mlmodels.model_dev.temporal_fusion_google No module named 'mlmodels.mode_tf' 
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+INFO:root:Using CPU
+mlmodels.model_dev.__init__
+mlmodels.model_gluon.gluonts_model_old
+mlmodels.model_gluon.util
+mlmodels.model_gluon.__init__
+mlmodels.model_gluon.util_autogluon
+mlmodels.model_gluon.fb_prophet
+
+  Error mlmodels.model_gluon.gluonts_model invalid syntax (gluonts_model.py, line 203) 
+mlmodels.model_gluon.gluon_automl
+
+  Error mlmodels.model_tch.util_data [Errno 2] File b'./data/train.csv' does not exist: b'./data/train.csv' 
+/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/mxnet/optimizer/optimizer.py:167: UserWarning: WARNING: New optimizer gluonnlp.optimizer.lamb.LAMB is overriding existing optimizer mxnet.optimizer.optimizer.LAMB
+  Optimizer.opt_registry[name].__name__))
+INFO:pytorch_transformers.modeling_bert:Better speed can be achieved with apex installed from https://www.github.com/nvidia/apex .
+INFO:pytorch_transformers.modeling_xlnet:Better speed can be achieved with apex installed from https://www.github.com/nvidia/apex .
+mlmodels.model_tch.matchZoo
+mlmodels.model_tch.util_transformer
+
+  Error mlmodels.model_tch.textcnn libtorch_cpu.so: cannot open shared object file: No such file or directory 
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:183: DeprecationWarning:
+
+invalid escape sequence \(
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:184: DeprecationWarning:
+
+invalid escape sequence \)
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:185: DeprecationWarning:
+
+invalid escape sequence \?
+
+INFO:transformers.file_utils:PyTorch version 1.2.0 available.
+Deprecaton set to False
+
+  {'model_uri': 'model_tf.1_lstm', 'learning_rate': 0.001, 'num_layers': 1, 'size': 6, 'size_layer': 128, 'output_size': 6, 'timestep': 4, 'epoch': 2} {'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year_small.csv', 'data_type': 'pandas', 'size': [0, 0, 6], 'output_size': [0, 6]} {'engine': 'optuna', 'method': 'prune', 'ntrials': 5} {'engine_pars': {'engine': 'optuna', 'method': 'normal', 'ntrials': 2, 'metric_target': 'loss'}, 'learning_rate': {'type': 'log_uniform', 'init': 0.01, 'range': [0.001, 0.1]}, 'num_layers': {'type': 'int', 'init': 2, 'range': [2, 4]}, 'size': {'type': 'int', 'init': 6, 'range': [6, 6]}, 'output_size': {'type': 'int', 'init': 6, 'range': [6, 6]}, 'size_layer': {'type': 'categorical', 'value': [128, 256]}, 'timestep': {'type': 'categorical', 'value': [5]}, 'epoch': {'type': 'categorical', 'value': [2]}} 
+
+  <module 'mlmodels.model_tf.1_lstm' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_tf/1_lstm.py'> 
+
+  ###### Hyper-optimization through study   ################################## 
+[W 2020-08-31 18:22:00,447] Setting status of trial#0 as TrialState.FAIL because of the following error: UnboundLocalError("local variable 'module' referenced before assignment",)
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
+    result = func(trial)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/bin/ml_optim", line 11, in <module>
+    load_entry_point('mlmodels', 'console_scripts', 'ml_optim')()
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 382, in main
+    test_json( path_json="template/optim_config_prune.json", config_mode= arg.config_mode )
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 230, in test_json
+    out_pars        = out_pars
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 57, in optim
+    out_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 170, in optim_optuna
+    study.optimize(objective, n_trials=ntrials)  # Invoke optimization of the objective function.
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 302, in optimize
+    gc_after_trial, None)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 538, in _optimize_sequential
+    self._run_trial_and_callbacks(func, catch, callbacks, gc_after_trial)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 550, in _run_trial_and_callbacks
+    trial = self._run_trial(func, catch, gc_after_trial)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
+    result = func(trial)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
+sh: 1: ml_mlmodels: not found
+mlmodels.model_tch.torchhub
+mlmodels.model_tch.__init__
+mlmodels.model_tch.transformer_sentence
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py  2>&1 | tee -a  cd log_.txt 
+os.getcwd /home/runner/work/mlmodels/mlmodels
+############ Your custom code ################################
+
+
+
+ python /home/runner/work/mlmodels/mlmodels/mlmodels/optim.py  
+Deprecaton set to False
+
+  {'model_uri': 'model_tf.1_lstm', 'learning_rate': 0.001, 'num_layers': 1, 'size': 6, 'size_layer': 128, 'output_size': 6, 'timestep': 4, 'epoch': 2} {'data_path': '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/timeseries/GOOG-year_small.csv', 'data_type': 'pandas', 'size': [0, 0, 6], 'output_size': [0, 6]} {'engine': 'optuna', 'method': 'prune', 'ntrials': 5} {'engine_pars': {'engine': 'optuna', 'method': 'normal', 'ntrials': 2, 'metric_target': 'loss'}, 'learning_rate': {'type': 'log_uniform', 'init': 0.01, 'range': [0.001, 0.1]}, 'num_layers': {'type': 'int', 'init': 2, 'range': [2, 4]}, 'size': {'type': 'int', 'init': 6, 'range': [6, 6]}, 'output_size': {'type': 'int', 'init': 6, 'range': [6, 6]}, 'size_layer': {'type': 'categorical', 'value': [128, 256]}, 'timestep': {'type': 'categorical', 'value': [5]}, 'epoch': {'type': 'categorical', 'value': [2]}} 
+
+  <module 'mlmodels.model_tf.1_lstm' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_tf/1_lstm.py'> 
+
+  ###### Hyper-optimization through study   ################################## 
+[W 2020-08-31 18:22:04,593] Setting status of trial#0 as TrialState.FAIL because of the following error: UnboundLocalError("local variable 'module' referenced before assignment",)
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
+    result = func(trial)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 394, in <module>
+    main()
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 382, in main
+    test_json( path_json="template/optim_config_prune.json", config_mode= arg.config_mode )
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 230, in test_json
+    out_pars        = out_pars
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 57, in optim
+    out_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 170, in optim_optuna
+    study.optimize(objective, n_trials=ntrials)  # Invoke optimization of the objective function.
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 302, in optimize
+    gc_after_trial, None)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 538, in _optimize_sequential
+    self._run_trial_and_callbacks(func, catch, callbacks, gc_after_trial)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 550, in _run_trial_and_callbacks
+    trial = self._run_trial(func, catch, gc_after_trial)
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/lib/python3.6/site-packages/optuna/study.py", line 569, in _run_trial
+    result = func(trial)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 143, in objective
+    module = model_create(module, model_pars, data_pars, compute_pars)  # module.Model(**param_dict)
+UnboundLocalError: local variable 'module' referenced before assignment
+
+
+
+ python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py    
+
+  #### Module init   ############################################ 
+
+  <module 'mlmodels.model_keras.textcnn' from '/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py'> 
+
+  #### Loading params   ############################################## 
+Using TensorFlow backend.
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py", line 258, in <module>
+    test_module(model_uri = MODEL_URI, param_pars= param_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/models.py", line 257, in test_module
+    model_pars, data_pars, compute_pars, out_pars = module.get_params(param_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py", line 165, in get_params
+    cf = json.load(open(data_path, mode='r'))
+FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/mlmodels/mlmodels/mlmodels/dataset/json/refactor/textcnn_keras.json'
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.11/x64/bin/ml_test", line 11, in <module>
+    load_entry_point('mlmodels', 'console_scripts', 'ml_test')()
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/ztest.py", line 655, in main
+    globals()[arg.do](arg)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/ztest.py", line 424, in test_pullrequest
+    raise Exception(f"Unknown dataset type", x)
+Exception: ('Unknown dataset type', '[W 2020-08-31 18:22:04,593] Setting status of trial#0 as TrialState.FAIL because of the following error: UnboundLocalError("local variable \'module\' referenced before assignment",)\n')
