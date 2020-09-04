58830b1df50c93011798b9df2ead32a428a25f4f
arita37 <noelkev0@gmail.com>
2020-09-04 15:21:30 +0000

    m_test_jupyter

--- log_jupyter/log_jupyter.py
+++ log_jupyter/log_jupyter.py
@@ -9,17 +9,17 @@
 
  ********************************************************************************************************************************************
 
- ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/fabbc5e3ca3e323a8ed377f3610ec624553426a0', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'fabbc5e3ca3e323a8ed377f3610ec624553426a0', 'workflow': 'test_jupyter'}
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/efcef8181592ea3c5266732d69f0e58bba03164e', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'efcef8181592ea3c5266732d69f0e58bba03164e', 'workflow': 'test_jupyter'}
 
  ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_jupyter
 
  ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
 
- ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/fabbc5e3ca3e323a8ed377f3610ec624553426a0
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/efcef8181592ea3c5266732d69f0e58bba03164e
 
- ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/fabbc5e3ca3e323a8ed377f3610ec624553426a0
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/efcef8181592ea3c5266732d69f0e58bba03164e
 
- ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/fabbc5e3ca3e323a8ed377f3610ec624553426a0
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/efcef8181592ea3c5266732d69f0e58bba03164e
 
  ************************************************************************************************************************
 /home/runner/work/mlmodels/mlmodels/mlmodels/example/
