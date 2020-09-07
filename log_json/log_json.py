669f09a4a1edaef7edd7d3d13908bdda6a8b3a7f
arita37 <noelkev0@gmail.com>
2020-09-07 14:22:49 +0000

    m_test_json.yml

--- log_json/log_json.py
+++ log_json/log_json.py
@@ -9,17 +9,17 @@
 
  ********************************************************************************************************************************************
 
- ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '57ee956e8a43c3caf9a12dc405f3a3c828dc0130', 'workflow': 'test_json'}
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/b082256f6e4bc1246197e8a9a5a89f8800343b90', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'b082256f6e4bc1246197e8a9a5a89f8800343b90', 'workflow': 'test_json'}
 
  ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_json
 
  ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
 
- ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/b082256f6e4bc1246197e8a9a5a89f8800343b90
 
- ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/b082256f6e4bc1246197e8a9a5a89f8800343b90
 
- ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/b082256f6e4bc1246197e8a9a5a89f8800343b90
 
  ************************************************************************************************************************
 
