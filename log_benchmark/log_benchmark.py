5d8e4a5ec23b8aa728f7d4d22b60e7ed659551bf
arita37 <noelkev0@gmail.com>
2020-09-07 16:21:14 +0000

    m_test_benchmark.yml

--- log_benchmark/log_benchmark.py
+++ log_benchmark/log_benchmark.py
@@ -9,17 +9,17 @@
 
  ********************************************************************************************************************************************
 
- ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '57ee956e8a43c3caf9a12dc405f3a3c828dc0130', 'workflow': 'test_benchmark'}
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/afd5cb8c78dc189cea20f9c5604b35cadde8193f', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'afd5cb8c78dc189cea20f9c5604b35cadde8193f', 'workflow': 'test_benchmark'}
 
  ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_benchmark
 
  ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
 
- ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/afd5cb8c78dc189cea20f9c5604b35cadde8193f
 
- ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/afd5cb8c78dc189cea20f9c5604b35cadde8193f
 
- ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/57ee956e8a43c3caf9a12dc405f3a3c828dc0130
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/afd5cb8c78dc189cea20f9c5604b35cadde8193f
 
  ************************************************************************************************************************
 
