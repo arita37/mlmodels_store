6276b62a12c8da22aa1514f2c37e66e65cb3b1e9
arita37 <noelkev0@gmail.com>
2020-09-14 10:15:26 +0000

    m_test_all.yml

--- log_testall/log_testall.py
+++ log_testall/log_testall.py
@@ -25,9 +25,9 @@
 
   ############Check model ################################ 
 
-  ['model_gluon.gluonts_model', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.fb_prophet', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.charcnn_zhang', 'model_keras.charcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.armdn', 'model_keras.Autokeras', 'model_tch.transformer_sentence', 'model_tch.matchZoo', 'model_tch.textcnn', 'model_tch.torchhub'] 
+  ['model_tch.transformer_sentence', 'model_tch.textcnn', 'model_tch.torchhub', 'model_tch.matchZoo', 'model_gluon.gluonts_model_old', 'model_gluon.gluon_automl', 'model_gluon.fb_prophet', 'model_gluon.gluonts_model', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google', 'model_keras.armdn', 'model_keras.charcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.charcnn_zhang', 'model_keras.textcnn', 'model_keras.Autokeras', 'model_keras.deepctr'] 
 
-  Used ['model_gluon.gluonts_model', 'model_gluon.gluon_automl', 'model_gluon.gluonts_model_old', 'model_gluon.fb_prophet', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google', 'model_keras.deepctr', 'model_keras.textcnn', 'model_keras.charcnn_zhang', 'model_keras.charcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.armdn', 'model_keras.Autokeras', 'model_tch.transformer_sentence', 'model_tch.matchZoo', 'model_tch.textcnn', 'model_tch.torchhub'] 
+  Used ['model_tch.transformer_sentence', 'model_tch.textcnn', 'model_tch.torchhub', 'model_tch.matchZoo', 'model_gluon.gluonts_model_old', 'model_gluon.gluon_automl', 'model_gluon.fb_prophet', 'model_gluon.gluonts_model', 'model_tf.1_lstm', 'model_dev.temporal_fusion_google', 'model_keras.armdn', 'model_keras.charcnn', 'model_keras.namentity_crm_bilstm', 'model_keras.charcnn_zhang', 'model_keras.textcnn', 'model_keras.Autokeras', 'model_keras.deepctr'] 
 
 
 
@@ -35,11 +35,22 @@
 
  ********************************************************************************************************************************************
 
-  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon//gluonts_model.py 
-  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon//gluonts_model.py", line 203
-    if d ==  "single_dataframe" :
-                                ^
-SyntaxError: invalid syntax
+  python /home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py 
+
+  #### Loading params   ############################################## 
+
+  #### Loading dataset   ############################################# 
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 459, in <module>
+    test(pars_choice="json", data_path="model_tch/transformer_sentence.json", config_mode="test")
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 414, in test
+    Xtuple = get_dataset(data_pars)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch//transformer_sentence.py", line 219, in get_dataset
+    from mlmodels.dataloader import DataLoader
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/dataloader.py", line 318
+    else :
+         ^
+IndentationError: unindent does not match any outer indentation level
 Traceback (most recent call last):
   File "/opt/hostedtoolcache/Python/3.6.12/x64/bin/ml_test", line 11, in <module>
     load_entry_point('mlmodels', 'console_scripts', 'ml_test')()
