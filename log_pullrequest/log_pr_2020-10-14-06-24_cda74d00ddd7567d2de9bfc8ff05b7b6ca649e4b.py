32ca2b25d2c1961b7cf00e000d58205faee88313
arita37 <noelkev0@gmail.com>
2020-10-14 06:24:55 +0000

    m_pullrequest

---
+++ log_pullrequest/log_pr_2020-10-14-06-24_cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b.py
@@ -0,0 +1,92 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, name='ml_store', save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': 'cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/cda74d00ddd7567d2de9bfc8ff05b7b6ca649e4b
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  test_import 
+['model_sklearn.__init__', 'model_keras.armdn', 'model_keras.__init__', 'model_keras.namentity_crm_bilstm', 'model_keras.textcnn', 'model_keras.util', 'model_keras.charcnn', 'model_keras.charcnn_zhang', 'model_keras.Autokeras', 'model_keras.preprocess', 'model_keras.deepctr', 'model_tch.matchZoo', 'model_tch.util_data', 'model_tch.__init__', 'model_tch.textcnn', 'model_tch.transformer_sentence', 'model_tch.util_transformer', 'model_tch.torchhub', 'model_gluon.gluonts_model', 'model_gluon.__init__', 'model_gluon.gluonts_model_old', 'model_gluon.gluon_automl', 'model_gluon.util_autogluon', 'model_gluon.util', 'model_gluon.fb_prophet', 'model_dev.__init__', 'model_dev.temporal_fusion_google', 'template.00_template_keras', 'template.model_xxx', 'preprocess.tabular', 'preprocess.tabular_keras', 'preprocess.image', 'preprocess.ztemp', 'preprocess.__init__', 'preprocess.timeseries', 'preprocess.text', 'preprocess.generic', 'preprocess.text_keras', 'preprocess.text_torch', 'preprocess.generic_old', 'example.vision_mnist', 'example.lightgbm_glass', 'example.arun_model', 'example.arun_hyper', 'example.benchmark_timeseries_m4', 'example.benchmark_timeseries_m5', 'utils.ztest_structure', 'utils.test_dataloader', 'utils.parse', 'model_tf.1_lstm', 'model_tf.__init__', 'model_tf.util', 'model_rank.__init__']
+mlmodels.model_sklearn.__init__
+
+  Error mlmodels.model_keras.armdn invalid syntax (data.py, line 126) 
+mlmodels.model_keras.__init__
+
+  Error mlmodels.model_keras.namentity_crm_bilstm unindent does not match any outer indentation level (dataloader.py, line 318) 
+
+  Error mlmodels.model_keras.textcnn unindent does not match any outer indentation level (dataloader.py, line 318) 
+mlmodels.model_keras.util
+mlmodels.model_keras.charcnn
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset
+mlmodels.model_keras.charcnn_zhang
+
+  Error mlmodels.model_keras.Autokeras No module named 'autokeras' 
+
+  Error mlmodels.model_keras.preprocess cannot import name 'DenseFeat' 
+
+  Error mlmodels.model_keras.deepctr cannot import name 'DenseFeat' 
+mlmodels.model_tch.matchZoo
+
+  Error mlmodels.model_tch.util_data [Errno 2] File b'./data/train.csv' does not exist: b'./data/train.csv' 
+mlmodels.model_tch.__init__
+
+  Error mlmodels.model_tch.textcnn libtorch_cpu.so: cannot open shared object file: No such file or directory 
+
+Segmentation fault: 11
+
+Stack trace:
+  [bt] (0) /opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/mxnet/libmxnet.so(+0x3c27360) [0x7eff55f9b360]
