5d17b95b4b6a1bac41273e260e4a3455e8127998
arita37 <noelkev0@gmail.com>
2020-10-29 18:20:02 +0000

    m_pullrequest

---
+++ log_pullrequest/log_pr_2020-10-29-18-19_4cdc002cba85d4437aab96db0de9f52c658a62a5.py
@@ -0,0 +1,289 @@
+
+  test_pullrequest /home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json Namespace(config_file='/home/runner/work/mlmodels/mlmodels/mlmodels/config/test_config.json', config_mode='test', do='test_pullrequest', folder=None, log_file=None, name='ml_store', save_folder='ztest/') 
+
+  ml_test --do test_pullrequest 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '4cdc002cba85d4437aab96db0de9f52c658a62a5', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ************************************************************************************************************************
+
+  /home/runner/work/mlmodels/mlmodels/pullrequest/ 
+
+  ############Check model ################################ 
+
+  ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  Used ['/home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py'] 
+
+  ########### Run Check ############################## 
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+ ******** TAG ::  {'github_repo_url': 'https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5', 'url_branch_file': 'https://github.com/arita37/mlmodels/blob/dev/', 'repo': 'arita37/mlmodels', 'branch': 'dev', 'sha': '4cdc002cba85d4437aab96db0de9f52c658a62a5', 'workflow': 'test_pullrequest'}
+
+ ******** GITHUB_WOKFLOW : https://github.com/arita37/mlmodels/actions?query=workflow%3Atest_pullrequest
+
+ ******** GITHUB_REPO_BRANCH : https://github.com/arita37/mlmodels/tree/dev/
+
+ ******** GITHUB_REPO_URL : https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ******** GITHUB_COMMIT_URL : https://github.com/arita37/mlmodels/commit/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ******** Click here for Online DEBUGGER : https://gitpod.io/#https://github.com/arita37/mlmodels/tree/4cdc002cba85d4437aab96db0de9f52c658a62a5
+
+ ************************************************************************************************************************
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  test_import 
+['model_keras.namentity_crm_bilstm', 'model_keras.armdn', 'model_keras.Autokeras', 'model_keras.charcnn', 'model_keras.__init__', 'model_keras.deepctr', 'model_keras.preprocess', 'model_keras.util', 'model_keras.charcnn_zhang', 'model_keras.textcnn', 'example.arun_hyper', 'example.lightgbm_glass', 'example.benchmark_timeseries_m4', 'example.vision_mnist', 'example.arun_model', 'example.benchmark_timeseries_m5', 'template.model_xxx', 'template.00_template_keras', 'model_sklearn.__init__', 'model_gluon.fb_prophet', 'model_gluon.gluon_automl', 'model_gluon.__init__', 'model_gluon.gluonts_model_old', 'model_gluon.util_autogluon', 'model_gluon.util', 'model_gluon.gluonts_model', 'utils.test_dataloader', 'utils.ztest_structure', 'utils.model_v1', 'utils.predict', 'utils.train', 'utils.parse', 'utils.bayesian', 'preprocess.image', 'preprocess.generic', 'preprocess.__init__', 'preprocess.generic_old', 'preprocess.timeseries', 'preprocess.tabular_keras', 'preprocess.text_torch', 'preprocess.text_keras', 'preprocess.text', 'preprocess.ztemp', 'preprocess.tabular', 'model_dev.temporal_fusion_google', 'model_dev.__init__', 'model_rank.__init__', 'model_tch.torchhub', 'model_tch.matchZoo', 'model_tch.__init__', 'model_tch.util_transformer', 'model_tch.transformer_sentence', 'model_tch.util_data', 'model_tch.textcnn', 'model_tf.__init__', 'model_tf.util', 'model_tf.1_lstm']
+
+  Error mlmodels.model_keras.namentity_crm_bilstm unindent does not match any outer indentation level (dataloader.py, line 318) 
+
+  Error mlmodels.model_keras.armdn invalid syntax (data.py, line 126) 
+
+  Error mlmodels.model_keras.Autokeras No module named 'autokeras' 
+mlmodels.model_keras.charcnn
+mlmodels.model_keras.__init__
+
+  Error mlmodels.model_keras.deepctr cannot import name 'DenseFeat' 
+
+  Error mlmodels.model_keras.preprocess cannot import name 'DenseFeat' 
+mlmodels.model_keras.util
+/home/runner/work/mlmodels/mlmodels/mlmodels/dataset
+mlmodels.model_keras.charcnn_zhang
+
+  Error mlmodels.model_keras.textcnn unindent does not match any outer indentation level (dataloader.py, line 318) 
+
+  Error mlmodels.example.arun_hyper name 'copy' is not defined 
+
+  Error mlmodels.example.lightgbm_glass unindent does not match any outer indentation level (models.py, line 92) 
+mlmodels.example.benchmark_timeseries_m4
+
+  Error mlmodels.example.vision_mnist invalid syntax (vision_mnist.py, line 15) 
+
+  Error mlmodels.example.arun_model unindent does not match any outer indentation level (models.py, line 92) 
+
+  Error mlmodels.example.benchmark_timeseries_m5 [Errno 2] File b'./m5-forecasting-accuracy/calendar.csv' does not exist: b'./m5-forecasting-accuracy/calendar.csv' 
+
+  Error mlmodels.template.model_xxx invalid syntax (data.py, line 126) 
+
+  Error mlmodels.template.00_template_keras expected an indented block (00_template_keras.py, line 68) 
+Using TensorFlow backend.
+/opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/mxnet/optimizer/optimizer.py:167: UserWarning:
+
+WARNING: New optimizer gluonnlp.optimizer.lamb.LAMB is overriding existing optimizer mxnet.optimizer.optimizer.LAMB
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_gluon/gluonts_model_old.py:569: DeprecationWarning:
+
+invalid escape sequence \s
+
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+Using CPU
+mlmodels.model_sklearn.__init__
+mlmodels.model_gluon.fb_prophet
+mlmodels.model_gluon.gluon_automl
+mlmodels.model_gluon.__init__
+mlmodels.model_gluon.gluonts_model_old
+mlmodels.model_gluon.util_autogluon
+mlmodels.model_gluon.util
+
+  Error mlmodels.model_gluon.gluonts_model invalid syntax (gluonts_model.py, line 203) 
+
+  Error mlmodels.utils.test_dataloader unindent does not match any outer indentation level (dataloader.py, line 318) 
+mlmodels.utils.ztest_structure
+mlmodels.utils.model_v1
+
+  Error mlmodels.utils.predict No module named 'offline' 
+
+  Error mlmodels.utils.train No module named 'offline' 
+mlmodels.utils.parse
+
+  Error mlmodels.utils.bayesian invalid syntax (bayesian.py, line 650) 
+mlmodels.preprocess.image
+mlmodels.preprocess.generic
+mlmodels.preprocess.__init__
+mlmodels.preprocess.generic_old
+mlmodels.preprocess.timeseries
+
+  Error mlmodels.preprocess.tabular_keras cannot import name 'SparseFeat' 
+
+  Error mlmodels.preprocess.text_torch libtorch_cpu.so: cannot open shared object file: No such file or directory 
+mlmodels.preprocess.text_keras
+mlmodels.preprocess.text
+
+  Error mlmodels.preprocess.ztemp invalid character in identifier (ztemp.py, line 6) 
+mlmodels.preprocess.tabular
+
+  Error mlmodels.model_dev.temporal_fusion_google No module named 'mlmodels.mode_tf' 
+/home/runner/work/mlmodels/mlmodels/mlmodels/utils/model_v1.py:973: DeprecationWarning:
+
+invalid escape sequence \*
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic.py:740: DeprecationWarning:
+
+invalid escape sequence \(
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic.py:741: DeprecationWarning:
+
+invalid escape sequence \)
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic.py:742: DeprecationWarning:
+
+invalid escape sequence \?
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic_old.py:319: DeprecationWarning:
+
+invalid escape sequence \(
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic_old.py:320: DeprecationWarning:
+
+invalid escape sequence \)
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/generic_old.py:321: DeprecationWarning:
+
+invalid escape sequence \?
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/text_torch.py:86: DeprecationWarning:
+
+invalid escape sequence \(
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/text_torch.py:87: DeprecationWarning:
+
+invalid escape sequence \)
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/preprocess/text_torch.py:88: DeprecationWarning:
+
+invalid escape sequence \?
+
+Better speed can be achieved with apex installed from https://www.github.com/nvidia/apex .
+Better speed can be achieved with apex installed from https://www.github.com/nvidia/apex .
+PyTorch version 1.2.0 available.
+mlmodels.model_dev.__init__
+mlmodels.model_rank.__init__
+mlmodels.model_tch.torchhub
+mlmodels.model_tch.matchZoo
+mlmodels.model_tch.__init__
+mlmodels.model_tch.util_transformer
+mlmodels.model_tch.transformer_sentence
+
+  Error mlmodels.model_tch.util_data [Errno 2] File b'./data/train.csv' does not exist: b'./data/train.csv' 
+
+  Error mlmodels.model_tch.textcnn libtorch_cpu.so: cannot open shared object file: No such file or directory 
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:183: DeprecationWarning:
+
+invalid escape sequence \(
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:184: DeprecationWarning:
+
+invalid escape sequence \)
+
+/home/runner/work/mlmodels/mlmodels/mlmodels/model_tch/textcnn.py:185: DeprecationWarning:
+
+invalid escape sequence \?
+
+WARNING:tensorflow:From /opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+From /opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/tensorflow_core/python/compat/v2_compat.py:68: disable_resource_variables (from tensorflow.python.ops.variable_scope) is deprecated and will be removed in a future version.
+Instructions for updating:
+non-resource variables are not supported in the long term
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/bin/ml_optim", line 11, in <module>
+    load_entry_point('mlmodels', 'console_scripts', 'ml_optim')()
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/pkg_resources/__init__.py", line 490, in load_entry_point
+    return get_distribution(dist).load_entry_point(group, name)
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/pkg_resources/__init__.py", line 2854, in load_entry_point
+    return ep.load()
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/pkg_resources/__init__.py", line 2445, in load
+    return self.resolve()
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/lib/python3.6/site-packages/pkg_resources/__init__.py", line 2451, in resolve
+    module = __import__(self.module_name, fromlist=['__name__'], level=0)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 32, in <module>
+    from mlmodels.models import model_create, module_load
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/models.py", line 92
+    else :
+         ^
+IndentationError: unindent does not match any outer indentation level
+sh: 1: ml_mlmodels: not found
+mlmodels.model_tf.__init__
+mlmodels.model_tf.util
+mlmodels.model_tf.1_lstm
+
+
+
+
+
+ ********************************************************************************************************************************************
+
+  python /home/runner/work/mlmodels/mlmodels/pullrequest/aa_mycode_test.py  2>&1 | tee -a  cd log_.txt 
+os.getcwd /home/runner/work/mlmodels/mlmodels
+############ Your custom code ################################
+
+
+
+ python /home/runner/work/mlmodels/mlmodels/mlmodels/optim.py  
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/optim.py", line 32, in <module>
+    from mlmodels.models import model_create, module_load
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/models.py", line 92
+    else :
+         ^
+IndentationError: unindent does not match any outer indentation level
+
+
+
+ python /home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py    
+Using TensorFlow backend.
+Traceback (most recent call last):
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/model_keras/textcnn.py", line 31, in <module>
+    from mlmodels.dataloader import DataLoader
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/dataloader.py", line 318
+    else :
+         ^
+IndentationError: unindent does not match any outer indentation level
+Traceback (most recent call last):
+  File "/opt/hostedtoolcache/Python/3.6.12/x64/bin/ml_test", line 11, in <module>
+    load_entry_point('mlmodels', 'console_scripts', 'ml_test')()
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/ztest.py", line 655, in main
+    globals()[arg.do](arg)
+  File "/home/runner/work/mlmodels/mlmodels/mlmodels/ztest.py", line 424, in test_pullrequest
+    raise Exception(f"Unknown dataset type", x)
+Exception: ('Unknown dataset type', 'IndentationError: unindent does not match any outer indentation level\n')
